02a6b2ff18a50a97e513f2ed18a7b11d5f06dd53
Lívia Brito <analiviavicenar@gmail.com>
2024-05-01 18:08:50 -0300

    improving the login

--- login.php
+++ login.php
@@ -5,24 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tech Bridge</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
     <link rel="stylesheet" href="style2.css">
     <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-<nav>
+    <nav>
         <div>
-          <a class="navbar-brand" href="index.html"><img src="img/logo-t8.png" alt="logo TechBridge" srcset=""
-              class="logo"></a>
+            <a class="navbar-brand" href="index.html"><img src="img/logo-t8.png" alt="logo TechBridge" srcset="" class="logo"></a>
         </div>
-        <div style="padding-right: 30px;">
-        <h1>Login</h1>
-        </div>
-  </nav>
-  <hr style="border-top: 6px solid #006a63; margin: 0">
+    </nav>
+    <hr style="border-top: 6px solid #006a63; margin: 0">
     <?php
     if (isset($_GET['error'])) {
         $errorMessage = urldecode($_GET['error']);
@@ -34,11 +29,15 @@
     }
     ?>
     <form method="POST" action="action-login.php">
-        <label for="login">Login:</label>
-        <input type="text" name="login" id="login" required><br>
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" id="senha" required><br>
-        <button type="submit" id="submitBtn">Entrar</button>
+        <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="login" name="login" placeholder="name@example.com">
+            <label for="login">Email</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control" id="senha" name="senha" placeholder="Password">
+            <label for="senha">Senha</label>
+            <button type="submit" id="submitBtn">Entrar</button>
+        </div>
     </form>
     <script>
         // Aguarda 2 segundos (2000 milissegundos) antes de ocultar a div
@@ -49,9 +48,7 @@
             document.getElementById('success-message').style.display = 'none';
         }, 2000);
     </script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
